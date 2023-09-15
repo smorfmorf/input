c8c3449902fe52e602d64947a079304215809b63
max <malalamobilelegends@gmail.com>
2023-09-16 02:02:31 +1000

    ''

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const input = document.querySelector("input");
 const div = document.querySelector("div");
+let timeoutId;
 
 input.addEventListener("input", () => {
-  setTimeout(() => {
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
     div.textContent = input.value;
   }, 300);
 });
